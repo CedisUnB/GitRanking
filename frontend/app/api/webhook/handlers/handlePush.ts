75444ce8b15989b91726bb0626cea167b0d230f6
Beatriz Nascimento <Beatriz.Nascimento@syngenta.com>
2026-04-06 17:20:06 -0300

    webhook handlers

---
+++ frontend/app/api/webhook/handlers/handlePush.ts
@@ -0,0 +1,11 @@
+async function handlePush(body: any) {
+  console.log("🚀 Push received!");
+
+  const commits = body.commits;
+
+  commits.forEach((commit: any) => {
+    console.log("Commit:", commit.message);
+  });
+
+  return new Response("Push processed", { status: 200 });
+}
--- frontend/app/api/webhook/route.ts
+++ frontend/app/api/webhook/route.ts
@@ -1,8 +1,18 @@
 export async function POST(req: Request) {
   console.log("Received webhook:");
+  const event = req.headers.get("x-github-event");
 
   const body = await req.json();
   console.log(body);
+  switch (event) {
+    case "issues":
+      return handleIssue(body);
 
-  return new Response("OK", { status: 200 });
+    case "push":
+      return handlePush(body);
+
+    default:
+      console.log("Event not handled:", event);
+      return new Response("Ignored", { status: 200 });
+  }
 }
